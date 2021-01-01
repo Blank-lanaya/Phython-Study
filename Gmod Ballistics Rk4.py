db842df70de7235cc935992e7b78de8a892a8ff4
Youngbo Go <gyb357@naver.com>
2021-01-01 14:19:10 +0900

    Update Gmod Ballistics Rk4.py

--- Gmod Ballistics Rk4.py
+++ Gmod Ballistics Rk4.py
@@ -1,38 +1,53 @@
-dx = 10
+dx = 8
 dy = 2
 
-ground = box(pos = vector(0, 0, 0), size = vector(5, 0.2, 1), color = color.green)
-ball1  = sphere(pos = vector(0, 0, 0), radius = 0.2, color = color.yellow)
-ball2  = sphere(pos = vector(dx, dy, 0), radius = 0.2, color = color.red)
+box1 = box(pos = vector(0, 0, 0), size = vector(5, 0.2, 1), color = color.green)
+ball1 = sphere(pos = vector(0, 0, 0), radius = 0.2, color = color.yellow)
+ball2 = sphere(pos = vector(dx, dy, 0), radius = 0.2, color = color.red)
 
 calculatespeed = 1
-tickinterval = 0.015
 
-g = vector(0, -600, 0)
+gravity = 600
+g = vector(0, -gravity, 0)
+
 dragdiv  = 40
-
 caliber  = 120
 mass     = 0.01
 dragcoef = (pi*caliber**2)/(4000000*mass*dragdiv)
 
-muzzleVel = 100
+muzzlevel = 100
+
 dt = 0.0005
 m = -0.1
-increase = 1.5
 
+kp = 2
+ki = 0.75
+kd = 0.5
+pidDt = 0.1
 
 def toRad(th) : return th*(pi/180)
 def toDeg(th) : return th*(180/pi)
 def length(v1, v2) : return sqrt(v1**2 + v2**2)
-def toAngle(x, y) : return toDeg(atan(y/x))
 
-fth = toAngle(dx, dy)
-print("target angle: " + round(fth))
+def clamp(n, min, max) :
+  if n < min : n = min
+  if n > max : n = max
+  return n
 
-# Runge Kutta 4 Method
+# Quadratic formula solver
+def tth(mod) :
+  if mod == 1 :sign = 1
+  else : sign = -1
+  
+  root = (muzzlevel**4 - gravity*(gravity*dx**2 + 2*dy*muzzlevel**2))
+  tangent = (muzzlevel**2 + sign*sqrt(root))/(gravity*dx)
+  
+  return toDeg(atan(tangent))
+
+# Runge Kutta 4 method
 def rk4(th) :
   p = vector(0, 0, 0)
-  v = vector(muzzleVel*cos(th), muzzleVel*sin(th), 0)
+  v = vector(muzzlevel*cos(th), muzzlevel*sin(th), 0)
   i = 0
   while 1 :
     rate(calculatespeed*1000)
@@ -53,8 +68,6 @@ def rk4(th) :
     p = p + (dt/6)*(vel1 + 2*(vel2 + vel3) + vel4)
     v = v + (dt/6)*(acc1 + 2*(acc2 + acc3) + acc4)
     ball1.pos = p
-    i += 1
-    time = i*tickinterval
     
     limit = (dy/dx)*p.x + m
     
@@ -64,42 +77,47 @@ def rk4(th) :
   return endpos
 
 def solve(mod, tolerance) :
-  if mod == 1 :
-    faz   = 90
-    sign = -1
-    text = "high angle: "
-  else :
-    faz   = 0
-    sign = 1
-    text = "low angle: "
+  if mod == 1 : sign = -1
+  else : sign = 1
   
-  angle = faz
+  angle = tth(mod)
   theta = toRad(angle)
   
   loopcount = 0
+  i = preverr = 0
+  print("first angle:", angle)
   while abs(dx - ball1.pos.x) + abs(dy - ball1.pos.y) >= tolerance :
     endPos = rk4(theta)
     
     errX = dx - endPos.x
     errY = dy - endPos.y
-    cor = (errX + errY)*increase
+    err = clamp(errX + errY, -20, 20)
     
-    angle += cor*sign
-    theta = toRad(fth) + toRad(angle)
+    p = err
+    i = i + p*pidDt
+    d = (p - preverr)/pidDt
+    preverr = err
+    
+    pidcontrol = p*kp + i*ki + d*kd*0.1
+    
+    angle += pidcontrol*sign
+    theta = toRad(angle)
     
     if angle > 90 | dy > 0 & angle < 0 :
       print("can't solve")
       break
     
     loopcount += 1
-  
-  print(text + round(toDeg(theta)), "/", "loop: " + loopcount)
+    
+  print("optimal angle:", toDeg(theta), "/ loopcount:", loopcount)
+  return theta
 
 tolerance = 0.1
+mod = 0
 
-mod   = 0
 theta1 = solve(mod, tolerance)
 ball1.pos = vector(0, 0, 0)
 
-mod   = 1
+mod = 1
 theta2 = solve(mod, tolerance)
+ball1.pos = vector(0, 0, 0)
